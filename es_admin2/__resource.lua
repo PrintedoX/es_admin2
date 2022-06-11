d8f7db4e63b5ee4c1eae1312290ef9881e97bd20
PrintedoX <106436505+PrintedoX@users.noreply.github.com>
2022-06-12 00:48:39 +0200

    Add files via upload

---
+++ es_admin2/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'client.lua'
+
+server_scripts {
+	'server.lua',
+    'common.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+	'ui/index.html',
+	'ui/style.css'
+}
